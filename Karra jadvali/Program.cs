e683856abea05cd6e5e8e98d09509ff187c8631d
Jurabek <bkjurabek@gmail.com>
2023-04-07 18:01:40 +0500

    Добавьте файлы проекта.

---
+++ Karra jadvali/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Karra_jadvali
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //MohirDev platformasidagi DotNet kursida berib boriladigan topshiriqlar
+            //For loop sikli mavzusi bo'yicha "karra jadvali"
+            Console.WriteLine("Karra jadvali");
+
+            for(int a = 1; a <= 10; a++ )
+            {
+                for(int b = 1; b <= 10; b++ )
+                {
+                    Console.WriteLine($"{a} * {b} = {a * b}");
+                }
+                Console.WriteLine(" ");
+            }
+            Console.ReadKey();
+        }
+    }
+}
